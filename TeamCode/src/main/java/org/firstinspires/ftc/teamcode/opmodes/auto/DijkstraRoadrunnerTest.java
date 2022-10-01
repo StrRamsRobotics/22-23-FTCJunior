b76e1b2bbc5da0b1c71f4dd0b496b9c900d85f2d
newwares <newwares123@gmail.com>
2022-10-01 16:58:12 -0400

    remove recursion

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/DijkstraRoadrunnerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/DijkstraRoadrunnerTest.java
@@ -15,48 +15,35 @@ public class DijkstraRoadrunnerTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         DijkstraRoadrunnerTest.drive = new SampleTankDrive(hardwareMap);
         Graph graph = new Graph(); //field grid
-        Node[] start = new Node[]{
-                new Node(12, 12),
-                new Node(12, -12),
-                new Node(-12, 12),
-                new Node(-12, -12)
-        };
+        for (int i = -60; i <= 60; i += 24) {
+            for (int j = -60; j <= 60; j += 24) {
+                Node cur = new Node(i, j);
+                Node[] nexts = new Node[]{
+                        new Node(i + 24, j),
+                        new Node(i - 24, j),
+                        new Node(i, j + 24),
+                        new Node(i, j - 24)
+                };
+                for (Node next : nexts) {
+                    if (Math.abs(next.getY())<=60 && Math.abs(next.getX())<=60) {
+                        cur.addDestination(next);
+                        next.addDestination(cur);
+                        if (!graph.getNodes().contains(next)) {
+                            graph.addNode(next);
+                        } else {
+                            Node old = next;
+                            for (Node n : graph.getNodes()) {
+                                if (n.equals(next)) {
+                                    old = n;
+                                    break;
+                                }
+                            }
+                            old.getAdjacentNodes().putAll(next.getAdjacentNodes());
 
-        for (Node s : start) {
-            graph.addNode(s);
-            buildGraph(s, graph, new boolean[121][121]);
-        }
-    }
+                        }
 
-    private static void buildGraph(Node prev, Graph graph, boolean[][] vis) {
-        vis[60 + prev.getX()][60 + prev.getY()] = true;
-        Node[] nexts = new Node[]{
-                new Node(prev.getX() + 24, prev.getY()),
-                new Node(prev.getX() - 24, prev.getY()),
-                new Node(prev.getX(), prev.getY() + 24),
-                new Node(prev.getX(), prev.getY() - 24)
-        };
-        for (Node next : nexts) {
-            if (Math.abs(next.getX()) > 60 || Math.abs(next.getY()) > 60) {
-                continue;
-            }
-            prev.addDestination(next);
-            next.addDestination(prev);
-            if (!graph.getNodes().contains(next)) {
-                graph.addNode(next);
-            } else {
-                Node old = next;
-                for (Node n : graph.getNodes()) {
-                    if (n.equals(next)) {
-                        old = n;
-                        break;
                     }
                 }
-                old.getAdjacentNodes().putAll(next.getAdjacentNodes());
-
-            }
-            if (!vis[60 + next.getX()][60 + next.getY()]) {
-                buildGraph(next, graph, vis);
             }
         }
     }
--- Testing/src/main/java/com/testing/GraphTest.java
+++ Testing/src/main/java/com/testing/GraphTest.java
@@ -3,50 +3,38 @@ package com.testing;
 public class GraphTest {
     public static void main(String[] args) {
         Graph graph = new Graph();
-        Node[] start = new Node[]{
-                new Node(12, 12),
-                new Node(12, -12),
-                new Node(-12, 12),
-                new Node(-12, -12)
-        };
+        for (int i = -36; i <= 36; i += 24) {
+            for (int j = -36; j <= 36; j += 24) {
+                Node cur = new Node(i, j);
+                Node[] nexts = new Node[]{
+                        new Node(i + 24, j),
+                        new Node(i - 24, j),
+                        new Node(i, j + 24),
+                        new Node(i, j - 24)
+                };
+                for (Node next : nexts) {
+                    if (Math.abs(next.getY())<=36 && Math.abs(next.getX())<=36) {
+                        cur.addDestination(next);
+                        next.addDestination(cur);
+                        if (!graph.getNodes().contains(next)) {
+                            graph.addNode(next);
+                        } else {
+                            Node old = next;
+                            for (Node n : graph.getNodes()) {
+                                if (n.equals(next)) {
+                                    old = n;
+                                    break;
+                                }
+                            }
+                            old.getAdjacentNodes().putAll(next.getAdjacentNodes());
 
-        for (Node s : start) {
-            graph.addNode(s);
-            buildGraph(s, graph, new boolean[121][121]);
+                        }
+
+                    }
+                }
+            }
         }
         System.out.println(graph);
     }
-
-    private static void buildGraph(Node prev, Graph graph, boolean[][] vis) {
-        vis[60+prev.getX()][60+prev.getY()] = true;
-        Node[] nexts = new Node[]{
-                new Node(prev.getX() + 24, prev.getY()),
-                new Node(prev.getX() - 24, prev.getY()),
-                new Node(prev.getX(), prev.getY() + 24),
-                new Node(prev.getX(), prev.getY() - 24)
-        };
-        for (Node next : nexts) {
-            if (Math.abs(next.getX())>60||Math.abs(next.getY())>60) {
-                continue;
-            }
-            prev.addDestination(next);
-            next.addDestination(prev);
-            if (!graph.getNodes().contains(next)) {
-                graph.addNode(next);
-            } else {
-                Node old = next;
-                for (Node n : graph.getNodes()) {
-                    if (n.equals(next)) {
-                        old = n;
-                        break;
-                    }
-                }
-                old.getAdjacentNodes().putAll(next.getAdjacentNodes());
-
-            }
-                if (!vis[60+next.getX()][60+next.getY()]) {
-                    buildGraph(next, graph, vis);
-                }
-        }
-    }
+    
 }
