35f3efffc6ea26b2af2c6478f6d8da57f1235a58
newwares <newwares123@gmail.com>
2022-10-18 16:02:06 -0400

    oct 18

---
+++ MeepMeep/src/main/java/com/meepmeep/Dijkstra.java
@@ -0,0 +1,59 @@
+package com.testing;
+
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class Dijkstra {
+    private static Node getLowestDistanceNode(Set<Node> unsettledNodes) {
+        Node lowestDistanceNode = null;
+        double lowestDistance = Double.MAX_VALUE;
+        for (Node node : unsettledNodes) {
+            double nodeDistance = node.getDistance();
+            if (nodeDistance < lowestDistance) {
+                lowestDistance = nodeDistance;
+                lowestDistanceNode = node;
+            }
+        }
+        return lowestDistanceNode;
+    }
+
+    private static void calculateMinimumDistance(Node evaluationNode, Double edgeWeight, Node sourceNode) {
+        double sourceDistance = sourceNode.getDistance();
+        if (sourceDistance + edgeWeight < evaluationNode.getDistance()) {
+            evaluationNode.setDistance(sourceDistance + edgeWeight);
+            ArrayList<Node> shortestPath = new ArrayList<>(sourceNode.getShortestPath());
+            shortestPath.add(sourceNode);
+            evaluationNode.setShortestPath(shortestPath);
+        }
+    }
+
+    public static Graph calculateShortestPathFromSource(Graph graph, Node source) {
+        for (Node graphNode : graph.getNodes()) {
+            graphNode.setDistance(Double.MAX_VALUE);
+            graphNode.setShortestPath(new ArrayList<>());
+        }
+        source.setDistance(0.0);
+
+        Set<Node> settledNodes = new HashSet<>();
+        Set<Node> unsettledNodes = new HashSet<>();
+
+        unsettledNodes.add(source);
+
+        while (unsettledNodes.size() != 0) {
+            Node currentNode = getLowestDistanceNode(unsettledNodes);
+            unsettledNodes.remove(currentNode);
+            for (Entry<Node, Double> adjacencyPair : currentNode.getAdjacentNodes().entrySet()) {
+                Node adjacentNode = adjacencyPair.getKey();
+                double edgeWeight = adjacencyPair.getValue();
+                if (!settledNodes.contains(adjacentNode)) {
+                    calculateMinimumDistance(adjacentNode, edgeWeight, currentNode);
+                    unsettledNodes.add(adjacentNode);
+                }
+            }
+            settledNodes.add(currentNode);
+        }
+        return graph;
+    }
+}
